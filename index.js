86061cf19c3c60edfe243d2a68e0a38d3ca9fbd2
Shuaib O. Akanbi <shuaibakanbi@gmail.com>
2022-02-20 12:31:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Get all id
+const about = document.getElementById('about');
+const skills = document.getElementById('skills');
+const projects = document.getElementById('projects');
+const contacts = document.getElementById('contacts');
+
+
+
