9b1847e39a8cf4545f1815243b42c430d55bcc80
Shuaib O. Akanbi <shuaibakanbi@gmail.com>
2022-02-21 10:45:43 +0100

    modify project display and user interactivity

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const about = document.getElementById('about');
 const skills = document.getElementById('skills');
 const projects = document.getElementById('projects');
 const contacts = document.getElementById('contacts');
+const skillDetails = document.querySelectorAll('details');
 
 
+// skillDetails.addEventListener('scroll', ()=>{
+//  if(skillDetails.open) {
+//      skillDetails.setAttribute('hide')
+//  }
+// })
 
